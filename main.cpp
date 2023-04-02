8a7982b971578a9f346e8c9fe8826c64e646e563
Anna Berkowska <annaberkowska@protonmail.com>
2023-04-02 13:50:34 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,265 @@
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
+
+int main() {
+
+    char input[INPUT_SIZE];
+    char *data = (char *) malloc(DATA_SIZE * sizeof(char));
+    int sizeOfData = 0, currentIndex = 0, appendingAllowed = 1, parseData = 0;
+
+    // Inputs until a whitespace.
+    while (cin >> input) {
+
+        // Check if we invoke the commands ot input the data to the buffer.
+        if (strcmp(input, "????") == 0) {
+            appendingAllowed = 0;
+            parseData = 1;
+            continue;
+        }
+        else if (strcmp(input, "****") == 0) {
+            appendingAllowed = 1;
+            continue;
+        }
+
+        // Commands handling.
+        if (!appendingAllowed) {
+            if (parseData) {
+                // Data parsing.
+                printString(data);
+                parseData = 0;
+                // After we parse all data into according data structures we can erase the data buffer.
+                freeString(data, &currentIndex);
+            }
+            // Invoke the commands.
+            if (strcmp(input, "?") == 0) {
+                continue;
+            }
+            else {
+                // Parse the command.
+                char *commandParts[COMMAND_SIZE];
+                parseCommand(commandParts, input);
+                // Then invoke it.
+            }
+        }
+
+        // Append the data for parsing.
+        else
+            appendToBuffer(input, data, &sizeOfData, &currentIndex);
+    }
+
+    free(data);
+
+    // Initialize data blocks and sections.
+//    block *blocks;
+//    blocks = (block *) malloc(sizeof(block));
+//    section *sections;
+//    sections = (section *) malloc(sizeof(section));
+//    blocks->sectionArray[0] = sections;
+//    blocks->takenSections++;
+
+    // Free the allocated memory.
+//    free(sections);
+//    free(blocks);
+
+    return 0;
+}
+
+// STRING RELATED METHODS
+
+
+
+char **parseCommand(char *commandParts[], char *command) {
+    const char separator[SEPARATOR_SIZE] = ",";
+    // https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
+    commandParts[0] = strtok(command, separator);
+    int i = 0;
+    while( commandParts[i] != nullptr) {
+        cout << commandParts[i] << endl;
+        i++;
+        commandParts[i] = strtok(nullptr, separator);
+    }
+    return commandParts;
+}
+
+void freeString(char *data, int *currentIndex) {
+    memset(data, '\0', *currentIndex);
+    *currentIndex = 0;
+}
+
+void appendToBuffer(const char *input, char *data, int *sizeOfData, int *currentIndex) {
+    int j = 0;
+    while (input[j] > ' ') {
+        data[*currentIndex] = input[j];
+        (*currentIndex)++;
+        j++;
+        (*sizeOfData)++;
+    }
+    data[*currentIndex] = ' ';
+    (*currentIndex) ++;
+}
+
+void printString(char *data) {
+    int i = 0;
+    while (data[i] != '\0') {
+        cout << data[i];
+        i++;
+    }
+}
+
+// DATA PARSING RELATED METHODS
+
+int isGlobalAttribute(const char *data, int currentIndex) {
+    while (data[currentIndex] != '{' && data[currentIndex] != ';') {
+        currentIndex++;
+    }
+    if (data[currentIndex] == '{')
+        return 0;
+    else if (data[currentIndex] == ';')
+        return 1;
+    return 0;
+}
+
+//void parseData(block *blocks, section *sections, char *data, int size) {
+//    int currentIndex = 0;
+//    if (!isGlobalAttribute(currentIndex, data)) {
+//        if (getLast<block>(blocks) == nullptr) {
+//
+//        }
+//        else if (getLast<block>(blocks)->takenSections <= SECTIONS_PER_BLOCK) {
+//
+//        }
+//    }
+//    else {
+//
+//    }
+//}
+
+// LIST RELATED METHODS
+
+template <typename type> void printList(type *firstNode) {
+    type *temporary = firstNode;
+    while (temporary != nullptr) {
+        cout << temporary->data;
+        temporary = temporary->next;
+    }
+}
+
+template <typename type> int getListLength(type *firstNode) {
+    int length = 0;
+    type *temporary = firstNode;
+    while (temporary != nullptr) {
+        length += 1;
+        temporary = temporary->next;
+    }
+    return length;
+}
+
+template <typename type> type getFirst(type *firstNode) {
+    if (firstNode == nullptr)
+        return nullptr;
+    return firstNode;
+}
+
+template <typename type> type getAtPosition(type *firstNode, int position) {
+    type *temporary = firstNode;
+    while (temporary != nullptr) {
+        if (position == 0)
+            return temporary;
+        position -= 1;
+        temporary = temporary->next;
+    }
+    return nullptr;
+}
+
+template <typename type> type *getLast(type *firstNode) {
+    if (firstNode == nullptr)
+        return nullptr;
+    type *temporary = firstNode;
+    while (temporary->next != nullptr)
+        temporary = temporary->next;
+    return temporary;
+}
+
+template <typename type> type addFirst(type *firstNode, type *newNode) {
+    newNode->prev = nullptr;
+    newNode->next = firstNode;
+    if (firstNode != nullptr)
+        firstNode->prev = newNode;
+    return newNode;
+}
+
+template <typename type> type insertBefore(type *firstNode, type *newNode, type *node) {
+    if (node == nullptr)
+        return firstNode;
+    newNode->next = node;
+    newNode->prev = node->prev;
+    node->prev = newNode;
+    if (newNode->prev == nullptr)
+        return newNode;
+    newNode->prev->next = newNode;
+    return firstNode;
+}
+
+template <typename type> void insertAfter(type *newNode, type *node) {
+    if (node == nullptr)
+        return;
+    newNode->next = node->next;
+    newNode->prev = node;
+    if (node->next != nullptr)
+        node->next->prev = newNode;
+    node->next = newNode;
+}
+
+template <typename type> type addLast(type *firstNode, type *newNode) {
+    type *lastNode = getLast(firstNode);
+    newNode->prev = lastNode;
+    if (lastNode == nullptr)
+        return newNode;
+    lastNode->next = newNode;
+    return  firstNode;
+}
+
+template <typename type> type removeFirst(type *firstNode) {
+    return removeNode(firstNode, firstNode);
+}
+
+template <typename type> type removeNode(type *firstNode, type *node) {
+    if (node == nullptr)
+        return firstNode;
+    if (node->next != nullptr)
+        node->next->prev = node->prev;
+    if (node->prev == nullptr)
+        return node->next;
+    node->prev->next = node->next;
+    return firstNode;
+}
+
+template <typename type> void removeAfter(type *node) {
+    if (node == nullptr)
+        return;
+    if (node->next == nullptr)
+        return;
+    node->next = node->next->next;
+    if (node->next != nullptr)
+        node->next->prev = node;
+}
+
+template <typename type> type removeLast(type *firstNode) {
+    type *lastNode = getLast(firstNode);
+    return removeNode(firstNode, lastNode);
+}
+
+/*
+ * BLOCKS                     * ----------------- *
+ *                            |                   |
+ * SECTIONS                   [ * , * , * , * ] - [ * , * , * , * ]
+ *                             | | | | | | | |     | | | | | | | |
+ * SELECTORS                   * | * | * | * |     * | * | * | * |
+ * ATTRIBUTES                    * - * - * - *       * - * - * - *
+ *
+ * NOTE: ASTERISKS ARE NODES OF RESPECTIVE LISTS
+ */
+
---
+++ main.h
@@ -0,0 +1,107 @@
+#ifndef CSS_MAIN_H
+#define CSS_MAIN_H
+
+#include <iostream>
+#include <string.h>
+
+#define SECTIONS_PER_BLOCK 8
+#define INPUT_SIZE 256
+#define DATA_SIZE 4096
+#define COMMAND_SIZE 3
+#define SEPARATOR_SIZE 2
+
+enum type {
+    INT,
+    STRING
+};
+
+typedef struct block block;
+
+typedef struct section section;
+
+typedef struct node selector, attribute;
+
+typedef struct block {
+    section *sectionArray[SECTIONS_PER_BLOCK] = {};
+    int takenSections = 0;
+    block *prev = nullptr;
+    block *next = nullptr;
+} block;
+
+typedef struct section {
+    selector *selectorList = nullptr;
+    attribute *attributeList = nullptr;
+    section *prev = nullptr;
+    section *next = nullptr;
+} section;
+
+typedef struct node {
+    char *data[DATA_SIZE] = {};
+    node *prev = nullptr;
+    node *next = nullptr;
+} selector, attribute;
+
+// STRING RELATED METHODS
+
+//
+char **parseCommand(char *commandParts[], char *command);
+
+//
+void freeString(char *data, int *currentIndex);
+
+//
+void appendToBuffer(const char *input, char *data, int *sizeOfData, int *currentIndex);
+
+//
+void printString(char *data);
+
+// DATA PARSING RELATED METHODS
+
+//
+int isGlobalAttribute(const char *data, int currentIndex);
+
+//
+//void parseData(block *blocks, section *sections, char *data, int size);
+
+// LIST RELATED METHODS
+
+// Prints the data of all the nodes in the list.
+template <typename type> void printList(type *firstNode);
+
+// Returns the length of a list.
+template <typename type> int getListLength(type *firstNode);
+
+// Returns the first node of a list.
+template <typename type> type getFirst(type *firstNode);
+
+// Returns the node at a specific location of a list.
+template <typename type> type getAtPosition(type *firstNode, int position);
+
+// Returns the last node of a list.
+template <typename type> type *getLast(type *firstNode);
+
+//
+template <typename type> type addFirst(type *firstNode, type *newNode);
+
+//
+template <typename type> type insertBefore(type *firstNode, type *newNode, type *node);
+
+//
+template <typename type> void insertAfter(type *newNode, type *node);
+
+//
+template <typename type> type addLast(type *firstNode, type *newNode);
+
+//
+template <typename type> type removeFirst(type *firstNode);
+
+//
+template <typename type> type removeNode(type *firstNode, type *node);
+
+//
+template <typename type> void removeAfter(type *node);
+
+//
+template <typename type> type removeLast(type *firstNode);
+
+#endif //CSS_MAIN_H
